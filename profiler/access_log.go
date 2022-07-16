0c67a03d32f08b58a8f642e049197eee8664023f
myoan <motoki.yoan@gmail.com>
2022-07-16 17:25:10 +0900

    change path

--- profiler/access_log.go
+++ profiler/access_log.go
@@ -128,7 +128,7 @@ func (h *AccessLogHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 var (
-	kataribeCommandTmpl = `"/usr/local/bin/kataribe -h"`
+	kataribeCommandTmpl = `"/usr/bin/kataribe -h"`
 	// kataribeCommandTmpl = `"cat %s | kataribe -conf %s > %s"`
 )
 
