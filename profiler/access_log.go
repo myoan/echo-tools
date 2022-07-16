b64b880873ff21170e1ab397520b4e467d22f576
myoan <motoki.yoan@gmail.com>
2022-07-16 17:48:50 +0900

    sudo

--- profiler/access_log.go
+++ profiler/access_log.go
@@ -130,7 +130,7 @@ func (h *AccessLogHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 var (
 	// kataribeCommandTmpl = `hostname > %s`
 	// kataribeCommandTmpl = `"/usr/bin/kataribe -h"`
-	kataribeCommandTmpl = `cat %s | kataribe -conf %s > %s`
+	kataribeCommandTmpl = `sudo cat %s | kataribe -conf %s > %s`
 )
 
 func (h *AccessLogHandler) handle(ctx context.Context, body io.Reader) error {
